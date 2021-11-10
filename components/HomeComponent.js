0d596389eb26f1702d2ce90cf8531d48b41bf30b
Andrew Sutton <andrewsydsutton@gmail.com>
2021-11-09 18:48:46 -0600

    11.9 commit

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import Constants from "expo-constants"
-import { View, Text, StyleSheet, ImageBackground } from "react-native"
+import React, {Component} from 'react';
+import { View, Text, StyleSheet, ImageBackground, FlatList, Image } from "react-native"
+import BOOKS from "../booksData/BOOKS"
 
-const HomeComponent = () => {
-    return (
-        <ImageBackground source={{uri: "https://images.unsplash.com/photo-1604339454409-701c5278c546?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1827&q=80"}} resizeMode="cover" style={styles.image}>
-            <View styles={styles.container}>
-                <Text>HOME COMPONENT</Text>
-            </View>
-        </ImageBackground>
-    );
-    
+class HomeComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            books: BOOKS
+        }
+    }
+
+    render(){
+        const Item = ({title}) => {
+            return (
+                <View>
+                    <Text>{title}</Text>
+                </View>
+            )
+        } 
+
+        const renderBooks = ({item}) => {
+            return (
+                <View>
+                    <Item title={item.title}/>
+                </View>
+            )
+        }
+        return (
+            <ImageBackground source={{uri: "https://images.unsplash.com/photo-1604339454409-701c5278c546?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1827&q=80"}} resizeMode="cover" style={styles.image}>
+                <View styles={styles.container}>
+                    <FlatList
+                        data={this.state.books}
+                        renderItem={renderBooks}
+                        keyExtractor={item => item.id}
+                    >
+                    </FlatList>
+                </View>
+            </ImageBackground>
+        );    
+    }
 }
 const styles = StyleSheet.create({
     container: {
